edd244d2024ad7d8c6774f8df9883216ac206e3c
bmorris213 <54776139+bmorris213@users.noreply.github.com>
2024-08-26 10:04:30 -0600

    added triangle click function
    wrote triangle click function for game manager to add triangles per click

--- Triangle Clicker/Assets/Scripts/GameManager.cs
+++ Triangle Clicker/Assets/Scripts/GameManager.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using TMPro;
+using System;
+
+// game manager keeps track of all the stats and resources used in the game
+// by delegating to various other managers
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // player resources
+    public double triangleCount = 0;
+    public double triangleProduction = 0;
+    public double clickPower = 0.1;
 
-    // Update is called once per frame
-    void Update()
+    // public game objects
+    public TMP_Text triangleCountText;
+    public TMP_Text triangleProductionText;
+    public TMP_Text clickPowerText;
+
+    public void TriangleClick()
     {
-        
+        triangleCount += clickPower;
+        triangleCount = Math.Round(triangleCount, 2);
+        triangleCountText.text = "Triangles: " + triangleCount;
     }
 }
