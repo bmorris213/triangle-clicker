8a3f38fa28bb19d89c574b2d853f090aff32eb5b
bmorris213 <54776139+bmorris213@users.noreply.github.com>
2024-08-26 23:26:21 -0600

    Prefab Station
    Set up example behavior for default station for use in creating prefabs, fleshed out code for station behavior as well

--- Triangle Clicker/Assets/Scripts/GameManager.cs
+++ Triangle Clicker/Assets/Scripts/GameManager.cs
@@ -10,19 +10,53 @@ using System;
 public class GameManager : MonoBehaviour
 {
     // player resources
-    public double triangleCount = 0;
-    public double triangleProduction = 0;
-    public double clickPower = 0.1;
+    public double triangleCount;
+    public double triangleProduction;
+    public double clickPower;
 
     // public game objects
     public TMP_Text triangleCountText;
     public TMP_Text triangleProductionText;
     public TMP_Text clickPowerText;
 
+    // called once at startup
+    void Start()
+    {
+        // begin production function on 1 second timer
+        InvokeRepeating("AddProduction", 1f, 1f);
+    }
+
+    // called once each time program is given focus
+    void Awake()
+    {
+        // instanciate game manager
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // add production is the second to second production function
+    public void AddProduction()
+    {
+        triangleCount += triangleProduction;
+        triangleCount = Math.Round(triangleCount, 2); // correct floating point errors
+        triangleCountText.text = "Triangles: " + triangleCount;
+    }
+
+    // triangle click is used whenever the player clicks the big triangle
     public void TriangleClick()
     {
         triangleCount += clickPower;
-        triangleCount = Math.Round(triangleCount, 2);
+        triangleCount = Math.Round(triangleCount, 2); // correct floating point errors
         triangleCountText.text = "Triangles: " + triangleCount;
     }
+
+    // instance of game manager
+    public static GameManager Instance { get; private set; }
 }
--- Triangle Clicker/Assets/Scripts/StationManager.cs
+++ Triangle Clicker/Assets/Scripts/StationManager.cs
@@ -2,17 +2,105 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using TMPro;
+using System;
+
+// station manager is used by each station to control its function
 public class StationManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // station statistics
+    public int baseCost;
+    public int costIncreasePercent;
+    public int level;
+    public double effectAmount;
+    public string description;
+    public bool effectsTPS;
+    public bool multiplicative;
+
+    private double currentCost;
+
+    // public game objects
+    public TMP_Text currentCostText;
+    public TMP_Text levelText;
+    public TMP_Text effectText;
+    public TMP_Text descriptionText;
+
+    // called once at startup
     void Start()
     {
-        
+        currentCost = baseCost;
+        descriptionText.text = description;
+        effectText.text = effectAmount.ToString();
+        levelText.text = level.ToString();
+        currentCostText.text = baseCost.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    // called anytime the user attempts to purchase a station level
+    public void TryUpgrade()
     {
+        // verify user can afford upgrade
+        double currentTriangles = GameManager.Instance.triangleCount;
+
+        if (currentCost > currentTriangles)
+        {
+            return;
+        }
+
+        // update current triangle balance
+        currentTriangles -= currentCost;
+        currentTriangles = Math.Round(currentTriangles, 2); // correct floating point errors
+        GameManager.Instance.triangleCount = currentTriangles;
+        GameManager.Instance.triangleCountText.text = "Triangles: " + currentTriangles.ToString();
         
+        double effectResult = 0;
+
+        // apply effect
+        if (effectsTPS)
+        {
+            effectResult = GameManager.Instance.triangleProduction;
+        }
+        else
+        {
+            effectResult = GameManager.Instance.clickPower;
+        }
+        
+        if (multiplicative)
+        {
+            effectResult *= effectAmount;
+        }
+        else
+        {
+            effectResult += effectAmount;
+        }
+        
+        effectResult = Math.Round(effectResult, 2); // correct floating point errors
+        
+        if (effectsTPS)
+        {
+            GameManager.Instance.triangleProductionText.text = "Triangles per second: " + effectResult.ToString();
+        }
+        else
+        {
+            GameManager.Instance.clickPowerText.text = "Triangles per click: " + effectResult.ToString();
+        }
+
+        // update stats for station
+        level += 1;
+        levelText.text = level.ToString();
+
+        double totalEffect = effectAmount * level;
+        totalEffect = Math.Round(totalEffect, 2); // correct floating point errors
+        effectText.text = totalEffect.ToString();
+
+        currentCost = getCost();
+        currentCostText.text = currentCost.ToString();
+    }
+
+    // used to calculate costs for new station purchases
+    private double getCost()
+    {
+        double costIncreaseRate = 1 + (costIncreasePercent / 100);
+        double costAmount = baseCost * Math.Pow(costIncreaseRate, level);
+        return Math.Round(costAmount, 2);
     }
 }
